7c61a64e7da811dbb633dfcddc1f6b470824b6d2
hesteren <74653870+hesteren@users.noreply.github.com>
2022-10-04 12:14:25 +0200

    Changed default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,17 @@
 <template>
   <v-app dark>
-    <v-app-bar app></v-app-bar>
+    <v-app-bar app>
+      <v-app-bar-title>Kalipo</v-app-bar-title>
 
-    <v-main></v-main>
+      <v-spacer></v-spacer>
+
+      <v-btn to="/">Home</v-btn>
+      <v-btn to="/events">Events</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <Nuxt></Nuxt>
+    </v-main>
 
     <v-footer></v-footer>
   </v-app>
