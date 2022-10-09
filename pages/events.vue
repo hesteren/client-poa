0af0a276848afbcbd0720ef7811595e3aa177c48
hesteren <74653870+hesteren@users.noreply.github.com>
2022-10-09 11:12:21 +0200

    Search working

--- pages/events.vue
+++ pages/events.vue
@@ -1,13 +1,9 @@
 <template lang="">
   <div>
+    <input type="text" v-model="search" />
     <v-container>
       <v-row>
-        <v-col
-          cols="6"
-          md="4"
-          v-for="event in this.$store.state.events.events"
-          :key="event.id"
-        >
+        <v-col cols="6" md="4" v-for="event in filteredEvents" :key="event.id">
           <EventCard class="elevation-1" :event="event"></EventCard>
         </v-col>
       </v-row>
@@ -19,8 +15,20 @@ import EventCard from "~/components/EventCard.vue";
 
 export default {
   name: "Events",
+  data() {
+    return {
+      search: "",
+    };
+  },
   components: {
     EventCard,
+  },
+  computed: {
+    filteredEvents() {
+      return this.$store.state.events.events.filter((event) =>
+        event.title.toLowerCase().includes(this.search.toLowerCase())
+      );
+    },
   },
 };
 </script>
