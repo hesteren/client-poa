a4f50a8d1d5a71da66667e82eb8f65d5b31c85c7
Skiroe <r.s.canak@gmail.com>
2022-10-15 14:58:07 +0200

    added styling finished custom data card changed theme to light mode

--- components/CreateEventForm.vue
+++ components/CreateEventForm.vue
@@ -1,5 +1,13 @@
 <template>
-  <v-card>
+
+  <v-container id="main-container" class="d-flex fill-height justify-center align-center">
+  <v-row>
+  <v-col sm="3"></v-col>
+
+  <v-col cols="4" sm="6">
+
+
+  <v-card id="main-card" outlined elevation="2">
     <!-- <div>
       <v-card-title left>Create a new event</v-card-title>
       <v-btn to="/events"></v-btn>
@@ -10,63 +18,67 @@
           <h3>Create a new event</h3>
         </v-col>
         <v-col class="text-right">
-          <v-btn to="/events"><v-icon>mdi-window-close</v-icon></v-btn>
+          <v-btn to="/events" icon elevation="2" ><v-icon to="/events">mdi-window-close</v-icon></v-btn>
         </v-col>
       </v-row>
     </v-container>
 
-    <v-form>
-      <v-card v-if="step == 1">
+    <v-form >
+      <v-card v-if="step == 1" class="card" elevation="0">
         <v-card-title>Event title and description</v-card-title>
-        <v-card-text>Choose a unique title people will remember!</v-card-text>
-        <div>
-          <input
-            id="event-title-input"
-            v-model="event.title"
-            class="pl-4"
-            type="text"
-            placeholder="Event title"
-            maxlength="30"
-            required
-          />
-          <small>{{ this.event.title.length }} / 30</small>
-        </div>
-        <v-card-text>
-          Please write down a description about this new event! Make this
-          interesting for potential attendees.
+        <v-card-text>Choose a unique title and description people will remember! Make this interesting for potential attendees.
         </v-card-text>
-        <input
-          v-model="event.description"
-          class="pl-4"
-          type="text"
-          placeholder="Event description"
-          maxlength="300"
+
+        <v-col>
+
+        <v-text-field
+          id="event-title-input"
+          v-model="event.title"
+          label="Event title"
+          maxlength="30"
+          counter="30"
+          required
         />
-        <small>{{ this.event.description.length }} / 300</small>
-        <v-divider></v-divider>
+        </v-col>
+
+        <v-col>
+
+        <v-textarea
+          v-model="event.description"
+          label="Event description"
+          maxlength="300"
+          counter="300"
+          outlined
+          no-resize
+        />
+        </v-col>
+
       </v-card>
 
-      <v-card v-if="step == 2">
+      <v-card v-if="step == 2" elevation="0" class="card">
         <v-card-title>Event Image</v-card-title>
         <v-card-text>
-          Add an image to your event to lure people in! If no image is provided
-          , a default image will be used.
+          Add an image to your event to lure people in! If no image is provided a default image will be used.
         </v-card-text>
-        <v-file-input
-          truncate-length="100"
-          v-model="event.image"
-          show-size
-          accept="image/*"
-          outlined
-          prepend-icon="mdi-camera"
-          label="Upload an image"
-          required
-        >
-        </v-file-input>
-        <v-divider></v-divider>
+
+        <v-col>
+          <v-file-input
+            id="image-input"
+            truncate-length="100"
+            v-model="event.image"
+            show-size
+            accept="image/*"
+            outlined
+            prepend-icon="mdi-camera"
+            label="Upload an image"
+            required
+            style="margin-top: 70px;"
+          >
+          </v-file-input>
+        </v-col>
       </v-card>
 
-      <v-card v-if="step == 3">
+      <v-card v-if="step == 3" elevation="0" class="card">
         <v-card-title>Event Date's</v-card-title>
         <v-card-text>
           Here you can fill in the date and time of your event!
@@ -119,20 +131,20 @@
 
           </v-container>
 
-        <v-divider></v-divider>
       </v-card>
 
 
-      <v-card v-if="step === 4">
+      <v-card v-if="step === 4" elevation="0" class="card" >
 
         <v-card-title>Event data</v-card-title>
         <v-card-text>
-          Here you can add all the data people need for your event!
+          Here you can fill in the data about your event!
         </v-card-text>
 
 
         <v-col cols="12" sm="12">
           <v-text-field
+            dense
             v-model="event.capacity"
             label="Location"
             outlined
@@ -145,6 +157,7 @@
 
         <v-col cols="12" sm="12">
           <v-text-field
+            dense
             type="number"
             v-model="event.maxAttendees"
             label="Maximum number of attendees"
@@ -155,6 +168,7 @@
 
         <v-col cols="12" sm="12">
           <v-text-field
+            dense
             type="number"
             prepend-inner-icon="mdi-currency-eur"
             v-model="event.price"
@@ -167,44 +181,53 @@
 
       </v-card>
 
-      <v-card v-if="step === 5">
+      <v-card v-if="step === 5" elevation="0" class="card" id="card-4">
 
         <v-card-title>Custom Event data</v-card-title>
         <v-card-text>
-          Here you can add all the extra data your event needs!
+          Here you can add all the extra data your event needs! Fill in the type of the data and the value of data. You can add up to ten custom data fields!
         </v-card-text>
 
-        <v-container>
+        <v-container id="customdata-container">
 
-          <v-row v-for="count in this.customDataCount" :key="count">
+          <v-row v-for="data in this.event.customData" :key="data.id">
 
             <v-col cols="12" sm="5">
               <v-text-field
-                label="Key"
+                v-model="data.key"
+                label="Type"
                 outlined
                 maxlength="30"
                 counter="30"
-                hint="The key of the custom data, e.x. 'Duration'"
+                hint="The type of the custom data, e.x. 'Language'"
               ></v-text-field>
             </v-col>
 
             <v-col cols="12" sm="5">
               <v-text-field
+                v-model="data.value"
                 label="Value"
                 outlined
                 maxlength="30"
                 counter="30"
-                hint="The value of the custom data, e.x. '2 hours'"
+                hint="The value of the custom data, e.x. 'English'"
               ></v-text-field>
             </v-col>
 
             <v-col cols="12" sm="1">
-
+              <v-btn
+                icon
+                color="red"
+                @click="removeCustomData(data.id)"
+              >
+                <v-icon>mdi-window-close</v-icon>
+              </v-btn>
             </v-col>
 
           </v-row>
 
           <v-btn
+            v-if="customDataCount <= 10"
             class="mx-2"
             fab
             dark
@@ -224,25 +247,30 @@
 
       <!-- next steps here -->
 
-      <v-card>
         <v-card-actions>
-          <v-btn v-if="step != 1" @click="previousStep"
+          <v-btn  v-if="step != 1" @click="previousStep  "
             ><v-icon>mdi-arrow-left-thin</v-icon></v-btn
           >
           <v-spacer></v-spacer>
-          <v-btn v-if="step != steps" @click="nextStep"
+          <v-btn  v-if="step != steps" @click="nextStep"
             ><v-icon> mdi-arrow-right-thin </v-icon></v-btn
           >
 
-          <v-btn v-if="step == steps" @click="createEvent"
+          <v-btn id="sign-button" v-if="step == steps" @click="createEvent"
             >Sign &nbsp; <v-icon>mdi-draw</v-icon></v-btn
           >
         </v-card-actions>
-      </v-card>
     </v-form>
   </v-card>
+
+  </v-col>
+  </v-row>
+  </v-container>
+
 </template>
 <script>
+
+
 export default {
   name: "CreateEventForm",
   data() {
@@ -263,9 +291,13 @@ export default {
         maxAttendees: null,
         price: null,
 
-        data: {
-
-        },
+        customData: [
+          {
+            id: 1,
+            key: "",
+            value: "",
+          },
+        ],
       },
       locationRules: [
         (v) => !!v || "Location is required",
@@ -274,11 +306,7 @@ export default {
     };
   },
   created() {
-    let pe3 = "thePe3"
 
-    this.event.data[pe3] = "legit youssef"
-
-    console.log(this.event.data.thePe3)
   },
   methods: {
     nextStep() {
@@ -296,6 +324,8 @@ export default {
       this.step--;
     },
     addCustomData() {
+
+      // TODO: is a maximum necessary? (ask Peter)
       if (this.customDataCount > 10) {
         alert("The maximum number of custom data is 10");
         return
@@ -303,6 +333,16 @@ export default {
 
       this.customDataCount++;
 
+      this.event.customData.push({
+        id: this.customDataCount,
+        key: "",
+        value: "",
+      });
+
+    },
+    removeCustomData(id) {
+      this.customDataCount--;
+      this.event.customData = this.event.customData.filter((data) => data.id != id);
     },
     createEvent() {
       // TODO: Set price to free if price is empty or 0
@@ -311,4 +351,30 @@ export default {
   },
 };
 </script>
-<style></style>
+<style scoped>
+
+  #main-container {
+    margin-top: 10vh;
+  }
+
+  .card {
+    min-height: 360px;
+    max-height: 360px;
+  }
+
+  #sign-button {
+    background-color: #0076bf;
+    color: white;
+  }
+
+  /*To enable scrolling in card if there  is multiple custom data pairs*/
+  #card-4 {
+    display: flex;
+    flex-direction: column;
+  }
+  #customdata-container {
+    flex-grow: 1;
+    overflow: auto;
+  }
+
+</style>
