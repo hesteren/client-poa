af3b0014e9092bd339a5bd5febacca26be2af5e1
hesteren <74653870+hesteren@users.noreply.github.com>
2022-10-19 15:59:15 +0200

    Styled search bar events

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,32 +1,37 @@
 <template lang="">
   <v-container>
-    <input
-      style="
-        border: 1px solid;
-        margin-top: 5px;
-        margin-left: 10px;
-        border-radius: 10px;
-        padding: 10px;
-      "
-      type="text"
+    <v-text-field
+      class="mt-4 ml-3"
+      style="width: 200px"
+      dense
       v-model="search"
-      placeholder="Search an event"
-    />
+      label="Search an event"
+    >
+    </v-text-field>
 
     <v-container>
+      <h1>{{ test }}</h1>
       <v-row>
         <v-col cols="6" md="4" v-for="event in filteredEvents" :key="event.id">
           <EventCard class="elevation-1" :event="event"></EventCard>
         </v-col>
       </v-row>
     </v-container>
-      <div id="createButton">
-         <v-btn to="/events/create" fab dark large color="primary" fixed right bottom>
+    <div id="createButton">
+      <v-btn
+        to="/events/create"
+        fab
+        dark
+        large
+        color="primary"
+        fixed
+        right
+        bottom
+      >
         <v-icon dark>{{ "mdi-plus" }}</v-icon>
-    </v-btn>
-  </div>
+      </v-btn>
+    </div>
   </v-container>
-
 </template>
 <script>
 import EventCard from "~/components/EventCard.vue";
