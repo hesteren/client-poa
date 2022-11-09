9f5b21e6a2d9f7454d857d4b40adda0b248e2a61
Skiroe <r.s.canak@gmail.com>
2022-11-09 21:39:36 +0100

    about me and online icon added

--- components/AboutMeCard.vue
+++ components/AboutMeCard.vue
@@ -1,7 +1,7 @@
 <template>
 
   <v-card class="mt-4" max-width="500">
-    <v-card-title>About Me</v-card-title>
+    <v-card-title>About me</v-card-title>
     <v-divider></v-divider>
     <v-card-text>
      {{this.user.about}}
--- components/UserCard.vue
+++ components/UserCard.vue
@@ -1,20 +1,20 @@
 <template>
-  <v-card max-width="300" elevation="0">
+  <v-card max-width="300" elevation="0" >
     <v-container>
       <v-row>
-        <v-col cols="12" md="3">
-          <v-avatar color="primary">
-              <span class="white--text text-h5">{{
+        <v-col cols="6" md="3" >
+          <v-avatar color="primary" height="60" width="60" >
+              <span class="white--text text-h3">{{
                   user.name.slice(0, 2).toUpperCase()
                 }}</span>
           </v-avatar>
         </v-col>
 
-        <v-col cols="12" md="6" class="pl-0">
-          <v-card-title class="pa-0" id="name">{{ user.username }}</v-card-title>
-          <v-card-text class="pa-0 green--text" id="description"
-          > <v-icon color="green" small >mdi-circle-slice-8</v-icon> Online</v-card-text
-          >
+        <v-col cols="6" md="6" class="pl-2">
+          <v-card-title class="pa-0" id="name">{{ user.name }}</v-card-title>
+          <v-card-text class="pa-0" id="name">@{{ user.username }}</v-card-text>
+          <v-card-text class="pa-0 green--text" id="description">
+          <v-icon color="green" small >mdi-circle-slice-8</v-icon> Online</v-card-text>
         </v-col>
       </v-row>
     </v-container>
