1aa86ff7d1a611ef773bb09a373c0ecddacb5886
Skiroe <r.s.canak@gmail.com>
2022-11-09 15:56:53 +0100

    tussen commit

---
+++ store/users.js
@@ -0,0 +1,15 @@
+export const state = () => ({
+  users: [
+    {
+      id: 1,
+      name: 'Peter Nobels',
+      username: 'peter_nobels',
+    },
+    {
+      id: 2,
+      name: 'Matthias',
+      username: 'Matthias',
+    },
+  ],
+
+})
