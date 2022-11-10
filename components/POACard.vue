387efa6bfc9a97a171e13de8eabf480c67e5f6e6
Kester van Leeuwen <kester@posonas.com>
2022-11-10 17:28:25 +0100

    material icons added

--- components/POACard.vue
+++ components/POACard.vue
@@ -14,33 +14,37 @@
     <v-dialog v-model="dialog" width="550">
         <v-card>
         <v-card-title class="text-h5 white--text primary">
-            POA view
+            <v-text class="ml-4">POA view</v-text>
         </v-card-title>
 
         <div class="ml-5 mr-12">
             <v-card-text>
-            Name: 
+            <v-icon>mdi-folder-text</v-icon>
+            <v-text class="ml-2">Name:</v-text>
             <span class="float-right">{{nft.name}}</span>
             </v-card-text>
         </div>
         <v-divider></v-divider>
         <div class="ml-5 mr-12">
             <v-card-text>
-            Issue date:
+            <v-icon>mdi-clipboard-text-clock</v-icon>
+            <v-text class="ml-2">Issue date:</v-text>
             <span class="float-right">{{nft.creationDate}}</span>
             </v-card-text>
         </div>
         <v-divider></v-divider>
         <div class="ml-5 mr-12">
             <v-card-text>
-            Image URL: 
+            <v-icon>mdi-image</v-icon>
+            <v-text class="ml-2">Image URL:</v-text>
             <a class="float-right" target="_blank" :href="nft.imageUrl">Link to image</a>
             </v-card-text>
         </div>
         <v-divider></v-divider>
         <div class="ml-5 mr-12">
             <v-card-text>
-            Event: 
+            <v-icon>mdi-calendar</v-icon>
+            <v-text class="ml-2">Event:</v-text>
             <a class="float-right" :href="`/events/${nft.eventId}`">{{nft.event}}</a>
             </v-card-text>
         </div>
